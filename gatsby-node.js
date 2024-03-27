a6f3ace395a8bc14664926111f8da20e66ae800d
Yuriy Baranov <40540592+Yuri2b@users.noreply.github.com>
2024-03-27 13:33:09 +0530

    fix: Loading posts from the CMS

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,9 +36,12 @@ exports.sourceNodes = async ({
     const readyPath = `/static/${folder}/${fileName}`;
     return readyPath;
   };
-  let allData = []; 
+  let allData = [];
+  const postsUrl = new URL(options.postUrl);
+  postsUrl.searchParams.set('filter[limit]', 100);
   while(true){
-    const response = await axios.get(options.postUrl);
+    postsUrl.searchParams.set('filter[skip]', allData.length);
+    const response = await axios.get(postsUrl.toString());
     const totalCount = response.headers['x-total-count'] || response.data.length; // fallback in case of old backend and pagination not available
     allData.push(...response.data);
     console.log(`Loaded ${allData.length} posts of ${totalCount}`);
