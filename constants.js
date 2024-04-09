4e372b567c6482f6603c44100475c3a263fd6b9b
Yuriy Baranov <yurinevermind@gmail.com>
2024-04-09 10:50:01 +0530

    Plugin refactoring
    - Extracting images and wrapping then to Gatsby nodes
    - Introducing new node types

---
+++ constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // GraphQL nodes type for entities with type "post"
+  POST_NODE_TYPE: "OnlineSalesPost",
+
+  // GraphQL node type for any other entitiy types
+  PAGE_NODE_TYPE: "OnlineSalesPage",
+
+  // GraphQL node type for image file
+  IMAGE_FILE_TYPE: "OnlineSalesImage",
+};
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,95 +1,133 @@
-const axios = require('axios');
-const matter = require('gray-matter');
-const fs = require('fs-extra');
-const path = require('path');
+const axios = require("axios");
+const matter = require("gray-matter");
 
-const { createRemoteFileNode  } = require(`gatsby-source-filesystem`)
+const extractImages = require("./extractImages");
+const processRemoteImage = require("./processRemoteImage");
+const {
+  PAGE_NODE_TYPE,
+  POST_NODE_TYPE,
+  IMAGE_FILE_TYPE,
+} = require("./constants");
 
-exports.onPreInit = () => console.log("Loaded gatsby-source-onlinesales plugin")
+exports.onPreInit = () =>
+  console.log("Loaded gatsby-source-onlinesales plugin");
 
-const POST_NODE_TYPE = `Post`
+const getNodeType = (type) =>
+  type === "post" ? POST_NODE_TYPE : PAGE_NODE_TYPE;
 
-exports.sourceNodes = async ({
-  actions,
-  createContentDigest,
-  createNodeId,
-  getCache,
-  cache,
+const getNodeLanguage = (
+  languageCode /* assuming ISO 639-1 or BCP 47 language code string format */
+) =>
+  typeof languageCode === "string" && !!languageCode
+    ? languageCode.toLowerCase().split("-")[0]
+    : "en";
 
-}, options) => {
-  const { createNode } = actions
-  const processRemoteImage = async (url, folder) => {
-    const nodeId = createNodeId(`RemoteImage-${url}`);
-    const fileNode = await createRemoteFileNode({
-        url,
-        parentNodeId: nodeId,
-        getCache,
-        createNode,
-        createNodeId,
-        cache,
-    });
-    const fileName = `${fileNode.name}${fileNode.ext}`;
-    const outputPath = path.join(process.cwd(), 'public', 'static', folder);
-    const filePath = path.join(outputPath, fileName);
-    await fs.ensureDir(outputPath);
-    await fs.copy(fileNode.absolutePath, filePath);
-    const readyPath = `/static/${folder}/${fileName}`;
-    return readyPath;
-  };
+const getNodeSlug = (slug) =>
+  slug != null ? slug.split("/").reverse()[0] : "";
+
+const parsePost = ({ body, ...attrs }) => {
+  const { content, data } = matter(body);
+
+  return { body: content, meta: { ...attrs, ...data } };
+};
+
+exports.sourceNodes = async (nodePluginArgs, options) => {
+  const { actions, createContentDigest, createNodeId, getCache, cache } =
+    nodePluginArgs;
+  const { createNode } = actions;
+
   let allData = [];
   const postsUrl = new URL(options.postUrl);
-  postsUrl.searchParams.set('filter[limit]', 100);
-  while(true){
-    postsUrl.searchParams.set('filter[skip]', allData.length);
+  postsUrl.searchParams.set("filter[limit]", 100);
+  while (true) {
+    postsUrl.searchParams.set("filter[skip]", allData.length);
     const response = await axios.get(postsUrl.toString());
-    const totalCount = response.headers['x-total-count'] || response.data.length; // fallback in case of old backend and pagination not available
+    const totalCount =
+      response.headers["x-total-count"] || response.data.length; // fallback in case of old backend and pagination not available
     allData.push(...response.data);
     console.log(`Loaded ${allData.length} posts of ${totalCount}`);
-    if (allData.length >= totalCount) {
+    if (totalCount == allData.length) {
       break;
     }
   }
 
   // loop through data and create Gatsby nodes
-  await Promise.all(allData.map(async post => {
-    //Put post information in frontmatter
-    const content = matter(post.body);
-    Object.assign(post, content.data);
-    delete post.body;
+  await Promise.all(
+    allData.map(async (post) => {
+      const { body, meta } = parsePost(post);
 
-    const nodeId = createNodeId(`${POST_NODE_TYPE}-${post.id}`);
-    try {
-    if (post.coverImageUrl){
-      if (post.coverImageUrl.startsWith('/api/')) {
-        const staticUrl = await processRemoteImage(options.prepareUrl(post.coverImageUrl), post.slug);
-        console.log(`Created ${staticUrl} image`);
-        post.coverImageUrl = staticUrl;
-      }
-    }
-    if (post.avatar){
-      if (post.avatar.startsWith('/api/')) {
-        const staticUrl = await processRemoteImage(options.prepareUrl(post.avatar), post.slug);
-        console.log(`Created ${staticUrl} image`);
-        post.avatar = staticUrl;
-      }
-    }
+      const nodeType = getNodeType(meta.type);
+      const nodeId = createNodeId(`${nodeType}-${meta.id}`);
 
-    createNode({
-      id: nodeId,
-      parent: null,
-      children: [],
-      internal: {
-        type: POST_NODE_TYPE,
-        contentDigest: createContentDigest(content.content),
-        mediaType: 'text/markdown',
-        content: matter.stringify(content.content, post),
-      },
+      try {
+        if (meta.coverImageUrl) {
+          if (meta.coverImageUrl.startsWith("/api/")) {
+            const { readyPath: staticUrl } = await processRemoteImage(
+              options.prepareUrl(meta.coverImageUrl),
+              nodeId,
+              nodePluginArgs
+            );
+            console.log(`Created ${staticUrl} cover image`);
+            meta.coverImageUrl = staticUrl;
+          }
+        }
+
+        if (meta.avatar) {
+          if (meta.avatar.startsWith("/api/")) {
+            const { readyPath: staticUrl } = await processRemoteImage(
+              options.prepareUrl(meta.avatar),
+              nodeId,
+              nodePluginArgs
+            );
+            console.log(`Created ${staticUrl} avatar image`);
+            meta.avatar = staticUrl;
+          }
+        }
+
+        const { modifiedBody, imageFileNodes } = await extractImages(
+          body,
+          options.apiUrl,
+          nodePluginArgs
+        );
+
+        createNode({
+          id: nodeId,
+          language: getNodeLanguage(meta.language),
+          slug: getNodeSlug(meta.slug),
+          parent: null,
+          children: [],
+          images: imageFileNodes,
+          internal: {
+            type: nodeType,
+            contentDigest: createContentDigest(modifiedBody),
+            mediaType: "text/markdown",
+            content: matter.stringify(modifiedBody, meta),
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+      console.log(`Created ${post.title} node ${nodeType}`);
     })
-  }
-  catch(e){
-    console.log(e);
-  }
-    console.log(`Created ${post.title} node`);
-}));
-  return
-}
+  );
+  return;
+};
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+
+  createTypes(`
+    type ${IMAGE_FILE_TYPE} implements Node {
+      staticPath: String
+      file: File @link
+    }
+    
+    type ${POST_NODE_TYPE} implements Node {
+      images: [${IMAGE_FILE_TYPE}] @link
+    }
+
+    type ${PAGE_NODE_TYPE} implements Node {
+      images: [${IMAGE_FILE_TYPE}] @link
+    }
+  `);
+};
